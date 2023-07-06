6b6ca32ea17cb58a0bafee76b3d2f3b395e8c409
BAKA-470 <vivekjoshi2267735@gmail.com>
2023-07-06 20:46:41 +0530

    Added layouts

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
 const port = 8000;
+const expressLayout = require('express-ejs-layouts');
 
+app.use(expressLayout);
 
 app.use('/', require('./routes'));
 
