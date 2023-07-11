b6b02d96b7714055869929f8a9fa0482d5dc6a29
BAKA-470 <vivekjoshi2267735@gmail.com>
2023-07-11 19:50:33 +0530

    Added Sign in and Sign up pages

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,3 +3,24 @@ module.exports.profile = function(req, res) {
         title: 'user1'
     });
 }
+
+// rendering the sign up page
+module.exports.signUp = function(req, res) {
+    return res.render('user_sign_up', {
+        title: 'Codial|Sign Up'
+    })
+};
+// rendering the sign in page
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title: 'Codial|Sign In'
+    })
+};
+// getting the signup data
+module.exports.create = function(req, res) {
+        // will work on it in a while
+    }
+    // creating a session while signing in
+module.exports.createSession = function(req, res) {
+    // will work on it in a while
+}
