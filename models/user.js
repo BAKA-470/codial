0e38bb7e3db06508647498b9e42c51ea98b78e79
BAKA-470 <vivekjoshi2267735@gmail.com>
2023-07-11 13:45:09 +0530

    Added Schema for the database

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [6, "Password should be at least 6 characters long"]
+
+    },
+    name: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true //automatically adds createdAt and updatedAt fields to the schema
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
