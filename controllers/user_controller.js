e888db07943a39fc40d8af65ce523af068d49e3a
BAKA-470 <vivekjoshi2267735@gmail.com>
2023-07-11 23:31:07 +0530

    Updated User_controller with working Sign up page

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,6 @@
+const User = require('../models/user');
+// const User = require('../models/user');
+
 module.exports.profile = function(req, res) {
     return res.render('user1', {
         title: 'user1'
@@ -18,9 +21,46 @@ module.exports.signIn = function(req, res) {
 };
 // getting the signup data
 module.exports.create = function(req, res) {
-        // will work on it in a while
+    if (req.body.password != req.body.confirm_password) {
+        console.log("Passwords do not match");
+        return res.redirect('back');
     }
-    // creating a session while signing in
+    User.findOne({ email: req.body.email })
+        .then(function(user) {
+            if (!user) {
+                User.create(req.body)
+                    .then((user, err) => {
+                        if (err) {
+                            console.log('Error in creating user while signing up');
+                            return;
+                        }
+                        return res.redirect('/users/sign-in');
+                    });
+                // User.create(req.body, function(err, user) {
+                //     if (err) { console.log('Error in creating user while signing up'); return }
+                //     return res.redirect('/users/sign-in')
+                // })
+            } else {
+                return res.redirect('back');
+            }
+        });
+    // User.findOne({ email: req.body.email }, function(err, user) {
+    // if (err) {
+    //     console.log('Error in finding the user');
+    //     return
+    // }
+    // if (!user) {
+    //     User.create(req.body, function(err, user) {
+    //         if (err) { console.log('Error in creating user while signing up'); return }
+
+    //         return res.redirect('/users/sign-in')
+    //     })
+    // } else {
+    //     return res.redirect('back');
+    // }
+};
+
+// creating a session while signing in
 module.exports.createSession = function(req, res) {
     // will work on it in a while
 }
